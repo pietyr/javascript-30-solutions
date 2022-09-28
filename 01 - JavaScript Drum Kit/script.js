1b2f503e353386e67ef1f8083c6c9a62c1e3651e
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2022-09-28 14:29:33 +0200

    Fixed errors when keydown not listed keys

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,6 +1,7 @@
 document.addEventListener(`keydown`, (e) => {
 	const keyCode = e.keyCode;
 	const div = document.querySelector(`div[data-key="${keyCode}"]`);
+	if (!div) return;
 	const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
 
 	div.classList.toggle(`playing`);
